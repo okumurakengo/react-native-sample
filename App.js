c1d009b4b0577da6fb072b47ff8acf319f2b4fb0
okumurakengo <okumurakengo1a@gmail.com>
2019-07-28 00:45:31 +0900

    試してみた

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import EmojiDict from './src/components/EmojiDict';
+
+export default class App extends Component {
+  render() {
+    return <EmojiDict />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
